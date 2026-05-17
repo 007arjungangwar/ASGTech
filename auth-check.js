b749c2b5b914cb759784003356c996cd73a11142
007arjungangwar <arjungangwar3@gmail.com>
2026-05-17 20:43:08 +0530

    Refine immersive learning layouts

--- auth-check.js
+++ auth-check.js
@@ -3,7 +3,7 @@
 const ASG_AUTH = {
     brand: "ASG Tech",
     loginPage: "login.html",
-    cacheName: "asg-tech-v17",
+    cacheName: "asg-tech-v18",
     publicPages: [
         "",
         "index.html",
