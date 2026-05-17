0995483b7348b45f9386a9a65d471b629648aebf
007arjungangwar <arjungangwar3@gmail.com>
2026-05-17 20:26:57 +0530

    Expand course reader workspace

--- auth-check.js
+++ auth-check.js
@@ -3,7 +3,7 @@
 const ASG_AUTH = {
     brand: "ASG Tech",
     loginPage: "login.html",
-    cacheName: "asg-tech-v16",
+    cacheName: "asg-tech-v17",
     publicPages: [
         "",
         "index.html",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "asg-tech-v16";
+const CACHE_NAME = "asg-tech-v17";
 const urlsToCache = [
     "/learning-with-arjun/",
     "/learning-with-arjun/index.html",
