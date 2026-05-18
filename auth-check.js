809d97d9f3230a081a92356984533677880420a2
007arjungangwar <arjungangwar3@gmail.com>
2026-05-19 00:19:53 +0530

    Fix full-width focus page layouts

--- auth-check.js
+++ auth-check.js
@@ -3,7 +3,7 @@
 const ASG_AUTH = {
     brand: "ASG Tech",
     loginPage: "login.html",
-    cacheName: "asg-tech-v22",
+    cacheName: "asg-tech-v24",
     publicPages: [
         "",
         "index.html",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "asg-tech-v23";
+const CACHE_NAME = "asg-tech-v24";
 const urlsToCache = [
     "/learning-with-arjun/",
     "/learning-with-arjun/index.html",
