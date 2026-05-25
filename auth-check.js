d3b9522df9d460c4e03eb4f0c58e75d1774460f0
007arjungangwar <arjungangwar3@gmail.com>
2026-05-26 02:45:33 +0530

    Polish admin assessment layout

--- auth-check.js
+++ auth-check.js
@@ -3,7 +3,7 @@
 const ASG_AUTH = {
     brand: "ASG Tech",
     loginPage: "login.html",
-    cacheName: "asg-tech-v59",
+    cacheName: "asg-tech-v60",
     publicPages: [
         "",
         "index.html",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "asg-tech-v59";
+const CACHE_NAME = "asg-tech-v60";
 const urlsToCache = [
     "/learning-with-arjun/",
     "/learning-with-arjun/index.html",
