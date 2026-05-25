466feeae42681cfa9c98c9ce3f13b98479c10747
007arjungangwar <arjungangwar3@gmail.com>
2026-05-26 03:00:46 +0530

    Improve learning analytics dashboard

--- auth-check.js
+++ auth-check.js
@@ -3,7 +3,7 @@
 const ASG_AUTH = {
     brand: "ASG Tech",
     loginPage: "login.html",
-    cacheName: "asg-tech-v60",
+    cacheName: "asg-tech-v61",
     publicPages: [
         "",
         "index.html",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "asg-tech-v60";
+const CACHE_NAME = "asg-tech-v61";
 const urlsToCache = [
     "/learning-with-arjun/",
     "/learning-with-arjun/index.html",
